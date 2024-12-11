a7548efca506771fd2b35afdc47d6ec4965123d6
yash041197 <94910158+yash041197@users.noreply.github.com>
2024-12-11 13:55:12 +0000

    Yash041197/patch 83357 readrress (#6)
    * reg adress
    
    * AddedHimanshu

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -7,7 +7,7 @@ import { Columns } from 'lucide-react';
 const team = [
   {
     name: "Vibhu Taneja",
-    role: "Director - IT Consulting and Finance",
+    role: "Director - Education Consulting and Finance",
     image: "/pic-1.jpg",
     link : "https://www.linkedin.com/in/vibhutaneja/",
     bio: "Leading VTSAI's vision of connecting global educational institutions. Passionate enterpreneur with over 2 decades of global experience in investment banking & Education sector. His journey has been driven by real world problems , fostering creativity & building sustainable ventures that leaves positive impact.",
@@ -17,8 +17,16 @@ const team = [
     name: "Viriti Taneja",
     role: "Director - Education Consulting & HR",
     image: "/vriti.png",
-    link : "https://www.linkedin.com/in/vibhutaneja/",
+    link : "https://www.linkedin.com/in/vriti-taneja-b2683921/",
     bio: "Leading with a commitment of delivering education excellence. She has over 15 years of experience in the education sector driving transformation through innovative ideas & techniques.",
+    email : "info@vtsaioverseas.com"
+  },
+  {
+    name: "Himanshu Arora",
+    role: "Business Manager ",
+    image: "/Himanshu.jpeg",
+    link : "",
+    bio: "Accomplished Business Manager with extensive experience in driving operational excellence and strategic growth across diverse industries. With a proven track record in team leadership, financial planning, and process optimization. A skilled communicator and negotiator, adept at building strong relationships with stakeholders and aligning business objectives to deliver sustained success.",
     email : "info@vtsaioverseas.com"
   },
   {
@@ -33,9 +41,9 @@ const team = [
     name : "Mohit ",
     role : "Consultant & Advisor",
     image : "/mohit.jpg",
-    link : "https://www.linkedin.com/in/yash-pandya-data/",
-    bio : "With a Master's in Data Science and over 5 years of experience as a Data Analyst and Developer, I am joining this company to contribute to data-driven decision-making, optimize IT processes, and support business growth. I am dedicated to collaborating across teams, exploring new ideas, and using my skills to deliver meaningful solutions that align with the company’s goals.",
-    email : ""
+    link : "https://www.linkedin.com/in/mohit-dahiya-7a32ba12a/",
+    bio : "With a Bachelor’s and Master’s in Hotel Management and currently pursuing a PhD in Hotel Management and Tourism, I bring a strong academic and industry foundation to the team. I have 8 years of experience as an Assistant Professor in India and the UK, along with 2 years of hands-on industry experience in the Maldives and India. My focus is on bridging hospitality education and real-world application, contributing unique insights to support the team’s vision for growth and innovation.",
+    email : "info@vtsaioverseas.com"
   },
   {
     name : "Yash Pandya",
